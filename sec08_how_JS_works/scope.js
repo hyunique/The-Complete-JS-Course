0b2ba619ebe60fd2534f0d226d38cc57430c1992
Elena <kunstvanelena@gmail.com>
2022-11-24 14:57:33 +0100

    added scope study code

--- sec08_how_JS_works/scope.js
+++ sec08_how_JS_works/scope.js
@@ -1,1 +1,22 @@
 'use strict';
+
+function calcAge(birthYear) {
+    const age = 2037 - birthYear;
+
+    function printAge() {
+        const output = `${firstName}, You are ${age}, born in ${birthYear}`
+        console.log(output);
+
+        if (birthYear >= 1981 && birthYear <= 1996) {
+            var millenial = true;
+            const str = `Oh, and you are a millenial, ${firstName}`
+            console.log(str);
+        }
+        console.log(millenial)
+    }
+    printAge()
+    return age;
+}
+
+const firstName = 'Elena';
+calcAge(1992);
