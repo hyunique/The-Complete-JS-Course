a37a365591a4758ebec7eaac8e52773f6b9c3a93
Elena <kunstvanelena@gmail.com>
2022-12-05 17:27:03 +0100

    wrote answers to challenge questions

--- sec09_Data_structures/120.js
+++ sec09_Data_structures/120.js
@@ -2,12 +2,11 @@
 // Coding Challenge #3
 
 /* 
-Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
-1. Create an array 'events' of the different game events that happened (no duplicates)
-2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
-3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
-4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
-      [FIRST HALF] 17: ⚽️ GOAL
+Let's continue with our football betting app! 
+This time, we have a map with a log of the events that happened during the game. 
+The values are the events themselves, and the keys are the minutes 
+in which each event happened (a football game has 90 minutes plus some extra time).
+
 GOOD LUCK 😀
 */
 
@@ -24,4 +23,28 @@ const gameEvents = new Map([
     [80, '⚽️ GOAL'],
     [92, '🔶 Yellow card'],
   ]);
-  
+
+
+
+//   1. Create an array 'events' of the different game events 
+// that happened(no duplicates)
+const events = new Set([...gameEvents.values()])
+console.log(events)
+
+// 2. After the game has finished, it was found that the yellow card from minute 64 was unfair.
+// So remove this event from the game events log.
+gameEvents.delete(64)
+console.log(gameEvents)
+
+// 3. Print the following string to the console:
+// "An event happened, on average, every 9 minutes"
+//   (keep in mind that a game has 90 minutes)
+console.log(`An event happened, on average, every ${90/gameEvents.size} minutes`)
+
+// 4. Loop over the events and log them to the console,
+//   marking whether it's in the first half or second half (after 45 min) of the game, 
+//   like this:
+//       [FIRST HALF] 17: ⚽️ GOAL
+for (const [key, value] of gameEvents) {
+  console.log(`${key < 46 ?`[FIRST HALF]`:`[SECOND HALF]` } ${key}: ${value}` )
+}
