46f2001956b890d25f87d12462a0eae57d7123d2
Elena <kunstvanelena@gmail.com>
2022-12-15 12:39:17 +0100

    added event handler to log in btn, implemented login

--- sec11_Arrays/app.js
+++ sec11_Arrays/app.js
@@ -72,29 +72,27 @@ const displayMovements = function (movements) {
       containerMovements.insertAdjacentHTML('afterbegin',html)  // beforeend makes order inverted in this case
     });
 }
-displayMovements(account1.movements)
 
 const calcDisplayBalance = function (movements) {
     const balance = movements.reduce((acc, mov) => acc + mov, 0);
     labelBalance.textContent = `${balance}€`
 }
 
-calcDisplayBalance(account1.movements);
 
-const calcDisplaySummary = function (movements) {
-    const incomes = movements
+const calcDisplaySummary = function (acc) {
+    const incomes = acc.movements
         .filter(mov => mov > 0)
         .reduce((acc, mov) => acc + mov, 0);
     labelSumIn.textContent = `${incomes}€`
 
-    const out = movements
+    const out = acc.movements
         .filter(mov => mov < 0)
         .reduce((acc, mov) => acc + mov, 0);
     labelSumOut.textContent = `${Math.abs(out)}€`
 
-    const interest = movements
+    const interest = acc.movements
         .filter(mov => mov > 0)
-        .map(deposit => deposit * 1.2 / 100)
+        .map(deposit => (deposit * acc.interestRate) / 100)
         .filter((int, i, arr) => {
             console.log(arr);
             return int >= 1;
@@ -102,7 +100,6 @@ const calcDisplaySummary = function (movements) {
         .reduce((acc, int) => acc + int, 0);
     labelSumInterest.textContent = `${interest}€`
 }
-calcDisplaySummary(account1.movements)
 
 
 const createUsernames = function (accs) {
@@ -115,7 +112,33 @@ const createUsernames = function (accs) {
     });
 };
 createUsernames(accounts)
-console.log(accounts)
+
+
+// Event Handler
+let currentAccount;
+btnLogin.addEventListener('click', function (e) {
+   // Prevent form from submitting
+    e.preventDefault();
+    currentAccount = accounts.find(acc => acc.userName === inputLoginUsername.value)
+    //Optional chaining. check pin only when currentAccount exists. 
+    //When account doesn't exist, it will give undefined instead of error
+    if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    // Display UI and message
+        labelWelcome.textContent = `Welcom back, ${currentAccount.owner.split(' ')[0]}`
+        containerApp.style.opacity = 100;
+        // Clear input fields
+        inputLoginUsername.value = inputLoginPin.value = '';
+        // Loose focus on input after log in
+        inputLoginPin.blur()
+        // Display movements
+        displayMovements(currentAccount.movements);
+        // Display balance
+        calcDisplayBalance(currentAccount.movements);
+        // Display summary
+        calcDisplaySummary(currentAccount);
+}
+
+})
 
 
 
