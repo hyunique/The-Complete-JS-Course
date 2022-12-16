15863df965df4d90f715636c399552dee0a0830f
Elena <kunstvanelena@gmail.com>
2022-12-16 15:42:02 +0100

    implemented sort method, event handler for btnSort

--- sec11_Arrays/app.js
+++ sec11_Arrays/app.js
@@ -58,9 +58,13 @@ const account1 = {
   const inputCloseUsername = document.querySelector('.form__input--user');
   const inputClosePin = document.querySelector('.form__input--pin');
   
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
     containerMovements.innerHTML = '';
-    movements.forEach(function (mov, i) {
+
+ // using slice method to copy the original array
+    const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+        
+    movs.forEach(function (mov, i) {
         const type = mov > 0 ? 'deposit' : 'withdrawal'
 
         const html = `
@@ -163,6 +167,17 @@ btnTransfer.addEventListener('click', function (e) {
     }
 });
 
+btnLoan.addEventListener('click', function (e) {
+    e.preventDefault();
+    const amount = Number(inputLoanAmount.value);
+    if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+       // Add movement
+        currentAccount.movements.push(amount);
+        updateUI(currentAccount);
+    }
+    inputLoanAmount.value =''
+})
+
 btnClose.addEventListener('click', function (e) {
     e.preventDefault();
     if (inputCloseUsername.value === currentAccount.userName &&
@@ -175,5 +190,13 @@ btnClose.addEventListener('click', function (e) {
     }
     inputCloseUsername.value = inputClosePin.value = '';
 
+})
+
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+    e.preventDefault();
+    // run this function only when sorted is true
+    displayMovements(currentAccount.movements, !sorted)
+    sorted = !sorted
 })
 
