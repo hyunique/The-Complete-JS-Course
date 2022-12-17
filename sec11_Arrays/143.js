ff9e7244db4676af26678ef80b6a311d7d9067ab
Elena <kunstvanelena@gmail.com>
2022-12-17 23:37:03 +0100

    added more examples for array methods(lec.166)

--- sec11_Arrays/143.js
+++ sec11_Arrays/143.js
@@ -149,4 +149,83 @@ console.log(arrDeep.flat(2))
 const overalBalance = accounts
     .flatMap(acc => acc.movements)
     .reduce(acc, mov => acc + mov, 0);
-    console.log(overalBalance3)
+console.log(overalBalance3)
+    
+
+///////////////////////////////////////////////////////////////////
+// 166. Array Methods Practice
+// Data
+const account1 = {
+    owner: 'Jonas Schmedtmann',
+    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+    interestRate: 1.2, // %
+    pin: 1111,
+  };
+  
+  const account2 = {
+    owner: 'Jessica Davis',
+    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+    interestRate: 1.5,
+    pin: 2222,
+  };
+  
+  const account3 = {
+    owner: 'Steven Thomas Williams',
+    movements: [200, -200, 340, -300, -20, 50, 400, -460],
+    interestRate: 0.7,
+    pin: 3333,
+  };
+  
+  const account4 = {
+    owner: 'Sarah Smith',
+    movements: [430, 1000, 700, 50, 90],
+    interestRate: 1,
+    pin: 4444,
+  };
+  
+const accounts = [account1, account2, account3, account4];
+  
+// 1.
+const bankDepositSum = accounts
+    .flatMap(acc => acc.movements)
+    .filter(mov => mov > 0)
+    .reduce((sum, cur) => sum + cur, 0);
+
+// 2. use case of accumulator of reduce method
+// const numDeposits1000 = account
+//     .flatMap(acc => acc.movements)
+//     .filter(mov => mov >= 1000).length;
+const numDeposits1000 = account
+    .flatMap(acc => acc.movements)
+    .reduce((count, cur) => (cur >= 1000 ? ++count : count), 0);
+    // count++ returns 0 here
+    
+// prefixed ++ operator
+let a = 10;
+console.log(a++); // this still logs 10, although it actually added 1 to 'a'
+console.log(a); // logs 11
+console.log(++a); // this logs 12. it works same in eg. callback in reduce method
+
+// 3. create new object with reduce method
+const sums = accounts
+    .flatMap(acc => acc.movements)
+    .reduce((sums, cur) => {
+        // cur > 0 ? sums.deposits += cur : sums.withdrawals += cur;
+        sums[cur > 0 ? 'deposits' : 'withdrawals'] += cur;
+    return sums;
+    }, { deposits: 0, withdrawals: 0 });
+
+
+// 4. convert string to title-case
+// eg. this is a nice title -> This Is a Nice Title
+const convertTitleCase = function (title) {
+    const exceptions = ['a', 'an', 'and', 'the', 'but', 'or', 'on', 'in', 'with'];
+
+    const titleCase = title
+        .toLowerCase()
+        .split(' ')
+        .map(word => exceptions.includes(word) ? word : word[0].toUpperCase() + word.slice(1))
+        .join(' ');
+    return titleCase;
+}
+console.log(convertTitleCase('this is a nice title'))
