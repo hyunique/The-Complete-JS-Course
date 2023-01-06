d5af0d1778d25957f55c9f3cdb7cbeed9ff008fd
Elena <kunstvanelena@gmail.com>
2023-01-06 15:59:49 +0100

    added eventlistener for tab container, menu fade animation

--- sec13_advancedDOM/script.js
+++ sec13_advancedDOM/script.js
@@ -5,6 +5,11 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal'); //=node list !=array
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1')
+const tabs = document.querySelectorAll('operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+const nav = document.querySelector('.nav')
 
 ///////////////////////////////////////
 // Modal window
@@ -65,6 +70,62 @@ document.querySelectorAll('.nav__link').forEach(function (el) {
         document.querySelector(id).scrollIntoView({ behavior: 'smooth' })
     })
 })*/
+
+///////////////////////////////////////
+// Tabbed Component
+
+tabsContainer.addEventListener('click', function (e) {
+    const clicked = e.target.closest('.operations__tab');
+  
+    // Guard clause
+    if (!clicked) return;
+  
+    // Remove active classes
+    tabs.forEach(t => t.classList.remove('operations__tab--active'));
+    tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+  
+    // Activate tab
+    clicked.classList.add('operations__tab--active');
+  
+    // Activate content area
+    document
+      .querySelector(`.operations__content--${clicked.dataset.tab}`)
+      .classList.add('operations__content--active');
+  });
+
+///////////////////////////////////////
+// Menu fade animation
+const handleHover = function (e) {
+    if (e.target.classList.contains('nav__link')) {
+        const link = e.target;
+        const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+        const logo = link.closest('.nav').querySelector('img');
+
+        siblings.forEach(el => {
+            if (el !== link) el.style.opacity = this;
+        });
+        logo.style.opacity = this;
+    }
+}
+
+ // Passing "argument" into handler
+ //mouseover is similar to mouseenter, but enter does not bubble
+ nav.addEventListener('mouseover',handleHover.bind(0.5))
+ nav.addEventListener('mouseover',handleHover.bind(1))
+// nav.addEventListener('mouseover', function (e) {
+//     handleHover(e, 0.5)
+// })
+// nav.addEventListener('mouseout', function (e) {
+//     handleHover(e, 1)
+//  })
+
+///////////////////////////////////////
+// Sticky navigation
+const initialCoords = section1.getBoundingClientRect()
+window.addEventListener('scroll', function () {
+    if (this.window.scrollY > initialCoords.top) nav.classList.add('sticky')
+    else nav.classList.remove('sticky');
+})
 
 
 //----------------------------------------------------//
