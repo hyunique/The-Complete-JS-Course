4cad5782bf91ba50666570e6ea47075e5f283784
Elena <kunstvanelena@gmail.com>
2023-01-23 22:39:06 +0100

    added module pattern example

--- sec17_modules/script.js
+++ sec17_modules/script.js
@@ -28,4 +28,41 @@ const lastPost = getLastPost();
 console.log(lastPost) // logs Promise{<pending>}
 
 const lastPost2 = await getLastPost(); //=lastPost.then(last=>console.log(last))
-console.log(lastPost2) // logs data after it has been loaded
+console.log(lastPost2); // logs data after it has been loaded
+
+
+//---------------------------------------------------//
+//274. The module pattern
+
+/* main goal of module pattern = to encapsulate functionality to have private data
+    Best way to acheiving this is by using function.*/
+
+const ShoppingCart2 = (function () {
+    const cart = [];
+    const shippingCost = 10;
+    const totalPrice = 237;
+    const totalQuantity = 23;
+
+    const addToCart = function (product, quantity) {
+        cart.push({ product, quantity });
+        console.log(`${quantity} ${product} added to cart`)
+    };
+
+    const orderStock = function (product, quantity) {
+        cart.push({ product, quantity });
+        console.log(`${quantity} ${product} ordered from supplier`)
+    };
+
+    return {
+        addToCart,
+        cart,
+        totalPrice,
+        totalQuantity, 
+        
+    }
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+
+
+import cloneDeep from './node_modules/lodash-es/cloneDeep'
